73fb00a5228fad1cec70235ad754a0588fb447d6
Dave Strus <dave@getfretless.com>
2019-09-07 08:24:39 -0400

    Add very basic queueing animation

--- src/Item.js
+++ src/Item.js
@@ -5,12 +5,36 @@ import { faBars, faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 import './Item.css'
 
 class Item extends Component {
+  state = {
+    queued: false,
+    hidden: false,
+  }
+
+  onClick = () => {
+    this.setState(
+      { queued: true },
+      () => {
+        window.setTimeout(
+          () => this.setState({ hidden: true }),
+          300
+        )
+      }
+    )
+  }
+
   render() {
     const patron = this.props.data
+    const queuedClass = this.state.queued ? 'queued' : ''
+    const hiddenClass = this.state.hidden ? 'hidden' : ''
+    const className = `Item ${queuedClass} ${hiddenClass}`
 
     return (
-      <div className="Item">
-        <img src={`/assets/${patron.id}.jpg`} alt={patron.name} />
+      <div className={className}>
+        <img
+          className="patron"
+          src={`/assets/${patron.id}.jpg`}
+          alt={patron.phone}
+        />
 
         <div>
           <div className="phone">{patron.phone}</div>
@@ -19,7 +43,7 @@ class Item extends Component {
           <p>KIOSK A</p>
         </div>
 
-        <button>
+        <button onClick={this.onClick}>
           <div>
             <FontAwesomeIcon icon={faBars} className="menu" />
             <FontAwesomeIcon icon={faPlusCircle} className="circle" />
