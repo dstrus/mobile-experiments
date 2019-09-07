e9b7182ebc5db8af35288c4a99b5471cbdf24c97
Dave Strus <dave@getfretless.com>
2019-09-07 11:43:42 -0400

    Add color to tag data

--- src/lib/patrons.js
+++ src/lib/patrons.js
@@ -3,81 +3,81 @@ export const patrons = [
     id: 1,
     phone: 'XXX-XX4-7609',
     tags: [
-      { number: '121', type: 'coat' },
-      { number: '67', type: 'coat' }
+      { number: '121', type: 'coat', color: '#084c61' },
+      { number: '67', type: 'coat', color: '#f4b027' }
     ]
   },
   {
     id: 2,
     phone: 'XXX-XX4-7609',
     tags: [
-      { number: '168', type: 'coat' },
-      { number: '59', type: 'coat' },
-      { number: '38', type: 'coat' },
+      { number: '168', type: 'coat', color: '#084c61' },
+      { number: '59', type: 'coat', color: '#68a21d' },
+      { number: '38', type: 'coat', color: '#f4b027' },
     ]
   },
   {
     id: 3,
     phone: 'XXX-XX4-7609',
     tags: [
-      { number: '31', type: 'coat' },
+      { number: '31', type: 'coat', color: '#68a21d' },
     ]
   },
   {
     id: 4,
     phone: 'XXX-XX4-7609',
     tags: [
-      { number: '160', type: 'coat' },
-      { number: '131', type: 'coat' },
-      { number: '117', type: 'coat' },
-      { number: '41', type: 'coat' },
+      { number: '160', type: 'coat', color: '#68a21d' },
+      { number: '131', type: 'coat', color: '#084c61' },
+      { number: '117', type: 'coat', color: '#084c61' },
+      { number: '41', type: 'coat', color: '#f4b027' },
     ]
   },
   {
     id: 5,
     phone: 'XXX-XX4-7609',
     tags: [
-      { number: '100', type: 'coat' },
-      { number: '234', type: 'coat' },
+      { number: '100', type: 'coat', color: '#084c61' },
+      { number: '234', type: 'coat', color: '#68a21d' },
     ]
   },
   {
     id: 6,
     phone: 'XXX-XX4-7609',
     tags: [
-      { number: '141', type: 'coat' },
-      { number: '118', type: 'coat' },
-      { number: '66', type: 'coat' },
+      { number: '141', type: 'coat', color: '#f4b027' },
+      { number: '118', type: 'coat', color: '#f4b027' },
+      { number: '66', type: 'coat', color: '#084c61' },
     ]
   },
   {
     id: 7,
     phone: 'XXX-XX4-7609',
     tags: [
-      { number: '34', type: 'coat' },
-      { number: '83', type: 'coat' },
+      { number: '34', type: 'coat', color: '#68a21d' },
+      { number: '83', type: 'coat', color: '#f4b027' },
     ]
   },
   {
     id: 8,
     phone: 'XXX-XX4-7609',
     tags: [
-      { number: '134', type: 'coat' }
+      { number: '134', type: 'coat', color: '#68a21d' }
     ]
   },
   {
     id: 9,
     phone: 'XXX-XX4-7609',
     tags: [
-      { number: '44', type: 'coat' },
-      { number: '173', type: 'coat' },
+      { number: '44', type: 'coat', color: '#084c61' },
+      { number: '173', type: 'coat', color: '#f4b027' },
     ]
   },
   {
     id: 10,
     phone: 'XXX-XX4-7609',
     tags: [
-      { number: '50', type: 'coat' }
+      { number: '50', type: 'coat', color: '#084c61' }
     ]
   },
 ]
