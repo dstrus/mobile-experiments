2aa0ffff84233ebbca85cf483a7c83da6bc40e5c
Dave Strus <dave@getfretless.com>
2019-09-09 13:45:02 -0400

    Remove patrons from search results when items are returned

--- src/reducers/searchResults.js
+++ src/reducers/searchResults.js
@@ -1,15 +1,21 @@
-import { ADD_TO_QUEUE } from '../actions'
+import { ADD_TO_QUEUE, RETURN_ALL_ITEMS } from '../actions'
 
 export default function searchResults(state = [], action) {
+  let i = 0
   switch(action.type) {
     case ADD_TO_QUEUE:
       const searchResults = [...state]
-      const i = searchResults.findIndex(searchResult => searchResult.id === action.payload.id)
+      i = searchResults.findIndex(searchResult => searchResult.id === action.payload.id)
       const patron = {...searchResults[i]}
       patron.isQueued = true
       searchResults.splice(i, 1, patron)
 
       return searchResults
+    case RETURN_ALL_ITEMS:
+      const patrons = [...state]
+      i = patrons.findIndex(patron => patron.id === action.payload.id)
+      patrons.splice(i, 1)
+      return patrons
     default:
       return state
   }
