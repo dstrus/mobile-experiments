5f4b8e86d38567ba0e75b7e4b1d4cb43f421d1e6
Dave Strus <dave@getfretless.com>
2019-09-10 13:16:54 -0400

    Remove unused inputs and attributes

--- src/PageSelector.js
+++ src/PageSelector.js
@@ -1,26 +1,16 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons'
-
-import Segment from './Segment'
-
 import './PageSelector.css'
 
 function PageSelector(props) {
   return (
     <div className="PageSelector">
-      <NavLink
-        to="/search"
-        first
-      >
+      <NavLink to="/search">
         <h3>Search</h3>
       </NavLink>
-      <NavLink
-        to="/queue"
-        last
-      >
+
+      <NavLink to="/queue">
         <h3>Queue</h3>
         {
           props.queue.length !== 0 && (
