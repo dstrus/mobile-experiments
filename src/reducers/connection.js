f04697681e7502032b3f1631a617d7e98cf3571a
Dave Strus <dave@getfretless.com>
2019-09-15 09:01:58 -0400

    Remove console.log()

--- src/reducers/connection.js
+++ src/reducers/connection.js
@@ -1,7 +1,6 @@
 import * as connectionActions from '../actions/constants'
 
 export default function connection(state = { state: '' }, action) {
-  console.log(`ACTION TYPE: ${action.type}`)
   switch (action.type) {
     case connectionActions.CONNECTION_OPENING:
       return {
